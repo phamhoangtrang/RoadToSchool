6c191d1168cff4e2a3a8b6375efaf36d771ccc83
Lê Quang Minh <157082841+minhleeee123@users.noreply.github.com>
2026-08-24 17:59:07 +0700

    test: modernize PHPUnit environment

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,17 +2,14 @@
 
 namespace Tests\Feature;
 
-use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testBasicTest()
+    use RefreshDatabase;
+
+    public function test_guests_are_redirected_to_login(): void
     {
         $response = $this->get('/');
 
--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,17 +2,11 @@
 
 namespace Tests\Unit;
 
-use Tests\TestCase;
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use PHPUnit\Framework\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testBasicTest()
+    public function test_basic_assertion(): void
     {
         $this->assertTrue(true);
     }
